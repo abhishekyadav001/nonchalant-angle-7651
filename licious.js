6928f902515a1dbb10d2ded97dcae115a9a8ae41
abhishekyadav <yadavabhishek2723@gmail.com>
2022-07-22 16:41:29 +0530

    login tab completed

--- licious.js
+++ licious.js
@@ -5,3 +5,16 @@ document.querySelector(".categories").addEventListener("mouseenter",()=>{
 document.querySelector(".headpoints").addEventListener("mouseleave",()=>{
     document.querySelector(".headpoints").style.display = "none"
 })
+
+document.querySelector(".login").addEventListener("mouseenter",()=>{
+    document.querySelector(".loginpage").style.display = "block"
+})
+
+document.querySelector(".loginpage").addEventListener("mouseleave",()=>{
+    document.querySelector(".loginpage").style.display = "none"
+})
+
+document.querySelector(".submitbtn").addEventListener("click",(event)=>{
+    event.preventDefault();
+document.querySelector(".login>p").innerText="Profile"
+})
