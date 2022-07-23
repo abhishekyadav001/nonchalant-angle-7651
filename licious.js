8bec67f0972b710e4bcf785d0743998fd6dd7a28
abhishekyadav <yadavabhishek2723@gmail.com>
2022-07-24 02:51:44 +0530

    login functionality done

--- licious.js
+++ licious.js
@@ -13,8 +13,19 @@ document.querySelector(".login").addEventListener("mouseenter",()=>{
 document.querySelector(".loginpage").addEventListener("mouseleave",()=>{
     document.querySelector(".loginpage").style.display = "none"
 })
-
 document.querySelector(".submitbtn").addEventListener("click",(event)=>{
     event.preventDefault();
+    let number=document.querySelector(".moble").value;
+    let otp=document.querySelector(".OTP").value;
+  if(number=="" && otp==""){
+    alert("Fill Information First");
+    return;
+  }else if(number!="" && otp==""){
+    alert("Enter OTP")
+    return;
+  }else if(otp!="" && number==""){
+    alert("Enter Mobile Number first")
+    return;
+  }
 document.querySelector(".login>p").innerText="Profile"
 })
